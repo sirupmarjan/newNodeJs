6578e4e0dadb56a760adb98202186e138fa19605
repl.it user <replituser@example.com>
2021-01-20 08:36:36 +0000

    add comments

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var http = require('http');
 var theDt = require('./theDate')
 
+//create a server object:
 http.createServer(function(req, res){
   res.writeHead(200, {'Content-Type':'text/html'});
-  res.write("the date : "+theDt.myDateTime());
-  res.end();
-}).listen(8080);
+  res.write("the date : "+theDt.myDateTime());//write a response to the client
+  res.end();//end the response
+}).listen(8080); //the server object listens on port 8080
